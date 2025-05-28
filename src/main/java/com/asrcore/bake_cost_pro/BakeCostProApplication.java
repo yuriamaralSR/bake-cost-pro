759f37b0a256f6b7d7cc0eb3356c14a22a5f15eb
Yuri Amaral Santos Rocha <yuriamaral.yas@gmail.com>
2025-05-28 12:49:39 -0300

    init: Initial Commit

---
+++ src/main/java/com/asrcore/bake_cost_pro/BakeCostProApplication.java
@@ -0,0 +1,13 @@
+package com.asrcore.bake_cost_pro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BakeCostProApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BakeCostProApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/asrcore/bake_cost_pro/BakeCostProApplicationTests.java
@@ -0,0 +1,13 @@
+package com.asrcore.bake_cost_pro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BakeCostProApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
